681d291a025e31e565f2b6a55f31ca7be20e162e
clmquaz <114813417+clmquaz@users.noreply.github.com>
2022-11-12 11:12:32 +0000

    Something.

--- lab_4_3.py
+++ lab_4_3.py
@@ -1,8 +1,9 @@
-import from модуль_физ_конст.py g
+from модуль_физ_конст import  g
 
-def uuu (m,h,v):
+def uuu (m=0,h=0,v=0):
   m = int(input("m = "))
   h = int(input("h = "))
   v = int(input("v = "))
   E = m*(v**2)/2+m*g*h
   print(f'полная механическая энергия тела = {E}')
+print(uuu())
