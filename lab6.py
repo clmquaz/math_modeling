e3ab41df892a1bcef67c51870d71f46b01d8b74b
clmquaz <114813417+clmquaz@users.noreply.github.com>
2024-06-11 13:42:51 +0000

    rt

--- lab6.py
+++ lab6.py
@@ -1,1 +1,14 @@
 a = []
+
+x = input('возраст пользователя')
+a.append(x)
+x = input('пол пользователя')
+a.append(x)
+x = input('имя пользователя')
+a.append(x)
+x = input('город пользователя')
+a.append(x)
+x = input('в каком классе пользователь учится')
+a.append(x)
+
+print(a)
