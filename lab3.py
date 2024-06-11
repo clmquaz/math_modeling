638ca908b016afcae6e7c843d9aca7ec2963726a
clmquaz <114813417+clmquaz@users.noreply.github.com>
2024-06-11 13:22:57 +0000

    m

---
+++ lab3.py
@@ -0,0 +1,8 @@
+import numpy as np
+x = 3
+
+y = np.sqrt(x**3) / (x**3+3/x) * (4*x**7-x**5) + 80 * np.sqrt(27*x**4+12*x**3-5*x**2+10)
+print(y)
+
+y = (3%2+int(16.7*4.32))/(14.5+31%12-int(x**3.4))
+print(y)
