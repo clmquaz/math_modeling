ddee63f334de98926582ad9f723afa3365f330ae
clmquaz <114813417+clmquaz@users.noreply.github.com>
2024-06-14 22:20:53 +0000

    d

--- lab4.py
+++ lab4.py
@@ -1,16 +1,12 @@
 import numpy as np
 
 
-
 N = int(input('N '))
 M = int(input('M '))
 
 trigonometry_array = np.zeros((N, M))
-print(trigonometry_array)
 
 for i in range(0, N, 1):
     for j in range(0, M, 1):
-        print(i)
-        print(j)
         trigonometry_array[i, j] = np.sin(np.pi/180*(N*i + M*j +1))
 
--- lab5.py
+++ lab5.py
@@ -1,14 +1,12 @@
-from lab4 import trigonometry_array as trig
+from lab4 import trigonometry_array
+from lab4 import N
 
+x = 0
+y = 1
+s=0
 for i in range(0, N, 1):
-    for j in range(0, M, 1):
-        print(i)
-        print(j)
-        trigonometry_array[i, j] = np.sin(np.pi/180*(N*i + M*j +1))
+        s = trigonometry_array[i, x]
+        trigonometry_array[i, x] = trigonometry_array[i, y]
+        trigonometry_array[i, y] = s
 
-
-for i in range(0, N, 1):
-        trigonometry_array[N, j] =
-
-
-
+print(trigonometry_array)
