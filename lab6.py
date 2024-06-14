e6b303f14fdaa282ae7358f3f004698036cb009a
clmquaz <114813417+clmquaz@users.noreply.github.com>
2024-06-14 15:28:11 +0000

    j,yjhm

--- lab6.py
+++ lab6.py
@@ -1,4 +1,14 @@
 a1 = []
-for i in range(1, 9, 1):
-    a1.append(1*i)
+for i in range(1, 10, 1):
+    for ii in range(1, 10, 1):
+        a1.append(i*ii)
 print(a1)
+
+r = 0
+aa1 = []
+for i in range(1, 10, 1):
+    for a in range(0, 9, 1):
+        r = i+9*a
+        aa1.append(a1[r-1])
+    print(aa1)
+    aa1.clear()
