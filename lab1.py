4b21bc4766ce83cdd58da9b3db439e9a2997e523
clmquaz <114813417+clmquaz@users.noreply.github.com>
2024-08-15 16:11:49 +0000

    hj

--- lab1.py
+++ lab1.py
@@ -1,2 +1,16 @@
-def a(p):
+def fun(n, nu, a):
+    if len(a)!=n:
+        a.append(nu)
+        fun(n, nu+1, a)
+    else:
+        return a
     
+def number(p):
+    q = 0
+    a = []
+    fun(p, 1, a)
+    map(lambda a, q: q+1 if 0 == p%a else q, a)
+    print(q)
+    s = lambda q: 'yes' if q==2 or q ==1 else 'no'
+    print(s(q))
+number(1)
